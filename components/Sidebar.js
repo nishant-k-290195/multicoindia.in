316ac7134d1042670c3c7d0b8b3ca9085eacc9da
Nishant Kumar <nishant290195.nkr@gmail.com>
2021-08-09 01:40:55 -0400

    6th commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,35 +1,20 @@
 import SidebarStyles from '../styles/Sidebar.module.css'
 import {BsBoxArrowInLeft} from 'react-icons/bs'
 import Link from 'next/link'
-import ReactTouchEvents from "react-touch-events";
 
 const Sidebar = (props) => {
   const {sidebarState, setSidebarState} = props
   return (
-    <div 
-      className={`${SidebarStyles.section} ${sidebarState? SidebarStyles.open:SidebarStyles.close}`}
-      onSwipe={ (direction)=> {
-        switch (direction) {
-          case "left":
-            setSidebarState(false)
-            break
-          case "right":
-            setSidebarState(true)
-            break
-          default:
-            return sidebarState
-        }
-      }}
-      >
+    <div className={`${SidebarStyles.section} ${sidebarState? SidebarStyles.open:SidebarStyles.close}`} >
       <BsBoxArrowInLeft onClick ={() => {setSidebarState(!sidebarState)}} className={SidebarStyles.menuIcon}/>
       <div className={SidebarStyles.container}>
         <Link href='/'><img src="logo.png" alt="logo" /></Link>
         <div className={SidebarStyles.nav}>
-          <Link href='/'>Home</Link>
-          <Link href='/about'>About</Link>
-          <Link href='/products'>Products & Services</Link>
-          <Link href='/career'>Career</Link>
-          <Link href='/contact'>Contact</Link>
+          <div onClick ={() => {setSidebarState(false)}}><Link href='/'>Home</Link></div>
+          <div onClick ={() => {setSidebarState(false)}}><Link href='/about'>About</Link></div>
+          <div onClick ={() => {setSidebarState(false)}}><Link href='/products'>Products & Services</Link></div>
+          <div onClick ={() => {setSidebarState(false)}}><Link href='/career'>Career</Link></div>
+          <div onClick ={() => {setSidebarState(false)}}><Link href='/contact'>Contact</Link></div>
         </div>
       </div>
     </div>
